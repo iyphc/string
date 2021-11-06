0bbcbb27347ac71ee156fd12b39a0d043d27e9e2
Wohrus <werhistudy@yandex.ru>
2021-11-06 21:20:49 +0300

    Changed strdub to strdup

--- string.h
+++ string.h
@@ -5,13 +5,13 @@
  * @brief Contains function prototypes
  */
 
-int myStrlen(char* source);
-int myStrcmp(char* string1, char* string2);
-int myMemcmp(char* string1, char* string2,  size_t n);
-char* myStrcpy(char* target, char* source);
-char* myStrchr(char *source, int ch);
-char* myStrdub(char* source);
+int myStrlen(char* source); 
+int myStrcmp(char* string1, char* string2); 
+int myMemcmp(char* string1, char* string2,  size_t n); 
+char* myStrcpy(char* target, char* source); 
+char* myStrchr(char *source, int ch); 
+char* myStrdup(char* source); 
 char* myStrstr(char* source, char* value);
-void* myMemchr(char* source, int a, size_t n);
+void* myMemchr(char* source, int a, size_t n); 
 void* myMemset(void *source, int c, size_t n);
 void *myMemcpy(void *target, const void *source, size_t n);
